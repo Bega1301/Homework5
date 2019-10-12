b412c0bb4023cfdb4f76da658272f2e78f08b17a
Bega1301 <begaiym13011985@gmail.com>
2019-10-12 17:34:09 +0600

    corrected2

--- src/com/company/MagicDoor.java
+++ src/com/company/MagicDoor.java
@@ -1,20 +1,31 @@
 package com.company;
 
 public class MagicDoor {
-
-          Hero [] heroes = new Hero[3];
-
-
-    public void setHeroes(Hero[] heroes) {
-        this.heroes = heroes;
-
-    }
+    Hero[] heroes = new Hero[4];
 
     public Hero[] getHeroes() {
-
         return heroes;
     }
+
+    Hero h1 = new Hero(250, "Physic", 30);
+    Hero h2 = new Hero(200, "Kinetic", 15);
+    Hero h3 = new Hero(150, "Magical", 10);
+    Hero h4 = new Hero(100, "Medical", 0);
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
